05ad017d0de3fd0036d287cdd3f187f4fe658a09
Kathrine <kath423a@stud.kea.dk>
2021-10-20 15:45:45 +0200

    getting the page works, not products

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //Variabler oprettes og defineres
-let products;
+// let products; it's not industry standard to define to many global variables, should be defined when you need them.
 let container = document.querySelector("#container");
 let temp = document.querySelector("template");
 
@@ -11,17 +11,26 @@ const link = "http://jordbaerstudio.com/jordbaerstudio/wordpress/wp-json/wp/v2/p
 //Her sendes en http anmodning via fetch til WordPress for at hente Json data
 async function hentData() {
   const respons = await fetch(link);
-  kager = await respons.json();
+  products = await respons.json();
   vis(products);
 }
 
 //Når Json data er modtaget, klones html templatet og indholdet fra Json lægges ind en for en
+// the product variable name is """again""" defined here, but it can be named whatever you like, but not products_array, because we will define that it in this function and so it's going to be reserved.
 function vis(products) {
+  // Here we say, products_array = product, and we will check next if it's an array or object.
+  let products_array = products;
+  // this checks if the parameter: products is NOT an array, if this is true we cast the products into an array, because only one product was returned from the fetch and that is an object.
+  if (Array.isArray(products) == false) {
+    products_array = [products];
+  }
+  console.log(products_array);
   container.innerHTML = "";
-  products.forEach((product) => {
+  products_array.forEach((product) => {
     console.log(product);
     const klon = temp.cloneNode(true).content;
-    klon.querySelector("img").src = product.billede.guid;
+    //// !!! tere was an error here, because the attribute you where trying to fetch didn't exists, I thought this would be the correct link to the picture, but it seems not. Not entirely sure where that attribute is.
+    klon.querySelector("img").src = product._links["wp:attachment"][0];
     klon.querySelector(".produkt_navn").textContent = product.title.rendered;
     klon.querySelector(".beskrivelse").innerHTML = product.content.rendered;
     container.appendChild(klon);
